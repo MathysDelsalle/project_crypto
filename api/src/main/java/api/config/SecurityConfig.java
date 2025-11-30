5cd1d35e173eab24b780a9e9b694908e04d38cfd
Mathys Delsalle <math.dels84@gmail.com>
2025-11-30 12:34:07 +0100

    API-Interne-fonctionelle

---
+++ api/src/main/java/api/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    //temporaire car j'ai dependency dans le pom xml donc j'ai un login qui pop quand je test si le front recoit bien mon json
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() 
+            );
+
+        return http.build();
+    }
+}
